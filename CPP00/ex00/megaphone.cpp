24d22c57039106732bef5c4f3945dd70b953f1ad
Nicolas Lee <niclee@f6r2s3.clusters.42paris.fr>
2025-09-07 18:30:08 +0200

    module00 ex00

---
+++ CPP00/ex00/megaphone.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cctype>
+
+int	main(int ac, char **av)
+{
+	int	i;
+	int j;
+
+	i = 1;
+	if (ac == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		return 0;
+	}
+	while (i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			std::cout << (char) std::toupper(av[i][j]);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
