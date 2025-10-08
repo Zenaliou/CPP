fd7a4d1175ad33a154bdd2e2e589ed1c343eb735
niclee <nico@Zenaliu.localdomain>
2025-10-08 10:21:35 +0200

    ex01

---
+++ Module01/ex01/main.cpp
@@ -0,0 +1,23 @@
+#include "Zombie.hpp"
+
+int main() 
+{
+    int N = 5;
+    std::string name = "Walker";
+    
+    std::cout << "Creating a horde of " << N << " zombies named " << name << std::endl;
+    
+    Zombie* horde = zombieHorde(N, name);
+    
+    if (horde) 
+	{
+        for (int i = 0; i < N; i++) 
+		{
+            std::cout << "Zombie " << i + 1 << ": ";
+            horde[i].announce();
+        }
+        std::cout << "\nDestroying the horde..." << std::endl;
+        delete[] horde;
+    }
+    return (0);
+}
